da9cadb07976939d7e46a8910f291355dc5483d8
Armine <armine.gyulazyan@gmail.com>
2024-04-29 15:50:52 +0400

    new features

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,33 +14,57 @@ function extraxt_num_part(num) {
     return parseInt(num.replace(/AMD$/, ""), 10)
 }
 
-function addition(){
 
-    let num_to_add = document.getElementById("quantity").innerHTML
+function addition(quantity_id, price_id){
+
+    let num_to_add = document.getElementById(quantity_id).innerHTML
     let changed = parseInt(num_to_add, 10)+1
-    document.getElementById("quantity").innerHTML = changed
+    document.getElementById(quantity_id).innerHTML = changed
     
-    price = extraxt_num_part(document.getElementById("price").getAttribute("data-initial-price"))
+    price = extraxt_num_part(document.getElementById(price_id).getAttribute("data-initial-price"))
     price *= changed
-    document.getElementById("price").innerHTML = price.toString() + "AMD"
+    document.getElementById(price_id).innerHTML = price.toString() + "AMD"
 }
 
-function subtraction(){
+
+function subtraction(quantity_id, price_id){
     
-    let num_to_sub = document.getElementById('quantity').innerHTML
+    let num_to_sub = document.getElementById(quantity_id).innerHTML
     let int_num = parseInt(num_to_sub, 10)
     if (int_num == 1 || int_num == 0){
-        document.getElementById("quantity").innerHTML = 0
+        document.getElementById(quantity_id).innerHTML = 0
         return
     }
     
     int_num -= 1
-    document.getElementById("quantity").innerHTML = int_num
+    document.getElementById(quantity_id).innerHTML = int_num
     
-    price = extraxt_num_part(document.getElementById("price").getAttribute("data-initial-price"))
+    price = extraxt_num_part(document.getElementById(price_id).getAttribute("data-initial-price"))
     price *= int_num
-    document.getElementById("price").innerHTML = price.toString() + "AMD"
+    document.getElementById(price_id).innerHTML = price.toString() + "AMD"
 }
+
+function cart_number_change(cart_num_id, quantity_id){
+
+    let cart_number = parseInt(document.getElementById(cart_num_id).innerHTML)
+    let order_quantity = parseInt(document.getElementById(quantity_id).innerHTML)
+    document.getElementById(cart_num_id).innerHTML = cart_number + order_quantity
+
+}
+
+function delete_item(item_id) {
+    elements = document.getElementById(item_id).innerHTML
+    elements = ''
+    document.getElementById(item_id).innerHTML = elements
+}
+
+function empty_list(list) {
+    elements = document.getElementById(list).innerHTML
+    elements = ''
+    document.getElementById(list).innerHTML = elements
+}
+
+
 
 
 
